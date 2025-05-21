d34b6734ef9fe67dc777e67f28fc3dbae1cf7913
NevilFlexion <nevilbhayani.flexion@gmail.com>
2025-05-21 09:30:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3806,4 +3806,5 @@ async def get_invoice(
 
     
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
